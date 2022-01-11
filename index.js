83f00f6dae15e462d63246720a63a9b44085753d
Kevin Wang <kevqwang@gmail.com>
2022-01-11 16:21:44 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-const record = [
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-    //...
-  ]
-
-  function superbowlWin(array) {
-    const winningGame = array.find(game => game.result === "W");
-    return winningGame.year;
-
-}
-
-function superbowlWin(array) {
-    for (const index of array) {
-        if (index.result === "W") {
-            return index.year
-        }
-    }
-}
+superbowlWin = (record) => {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
