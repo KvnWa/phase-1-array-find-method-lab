dcc363346559cb2b849bda709781a2c6b79e2971
Kevin Wang <kevqwang@gmail.com>
2022-01-11 13:26:05 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  function superbowlWin(array) {
+    const winningGame = array.find(game => game.result === "W");
+    return winningGame.year;
+
+}
+
+function superbowlWin(array) {
+    for (const index of array) {
+        if (index.result === "W") {
+            return index.year
+        }
+    }
+}
