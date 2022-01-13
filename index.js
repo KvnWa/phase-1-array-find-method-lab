bef0cf0cc4db050f5518d552952d7e7a14b1d87e
Kevin Wang <kevqwang@gmail.com>
2022-01-13 10:50:03 -0500

    commit tree 0dd22b4bf9510c62ad2960df2448b2f30dd82891

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-superbowlWin = (record) => {
-    const result = record.find( record => record.result === "W" );
-    return !!result ? result.year : undefined;
+// superbowlWin = (record) => {
+//     const result = record.find( record => record.result === "W" );
+//     return !!result ? result.year : undefined;
+//   }
+
+  // find will return the first item in the array that matches the passed through argument
+  
+  
+  
+  function superbowlWin(teamYears) {
+    
+    const winningRecord = teamYears.find(elementOne => elementOne.result === "W");
+    return winningRecord ? winningRecord.year : undefined
   }
+  console.log(superbowlWin(records));
+  
